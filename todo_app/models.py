38d29a7115a9f70077022ed5feebfb0f99cd13a1
Joseph Dubon <josephdubon@pm.me>
2021-04-07 22:16:42 -0700

    Created todo model

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+# Todos Model
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
